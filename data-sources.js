3b08ee00b47ff9b67ac11512030749ba94a28de6
Bradley, Ben <ben.bradley@sciencelogic.com>
2021-03-31 14:51:02 -0500

    fix about datasource

--- data-sources.js
+++ data-sources.js
@@ -1,9 +1,9 @@
-const { About } = require("./resources/About/data-source");
+const { AboutDataSource } = require("./resources/About/data-source");
 const { BookDataSource } = require("./resources/Book/data-source");
 const { UserDataSource } = require("./resources/User/data-source");
 
 const dataSources = () => ({
-  about: new About(),
+  about: new AboutDataSource(),
   books: new BookDataSource(),
   users: new UserDataSource()
 });
